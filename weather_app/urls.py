fd8a1f625d44fca8e57341656856bfa9e327c392
Ajaysingh1561 <ajay.kumar@habilelabs.io>
2025-07-27 00:18:41 +0530

    refactor code

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,9 +1,8 @@
 # core/urls.py
 from django.urls import path
-from .views import summary_view , fetch_climate_data
+from .views import summary_view, fetch_climate_data
 
 urlpatterns = [
-    # path('api/summary/', SummaryAPIView.as_view(), name='api-summary'),
-    path('summary/', summary_view, name='summary'),
+    path("", summary_view, name="summary"),
     path("fetch-climate-data/", fetch_climate_data),
 ]
--- weather_data/urls.py
+++ weather_data/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("weather_app.urls")),
+    path("", include("weather_app.urls")),
 ]
